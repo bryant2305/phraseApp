0ae7f2514e381de3d9e844274107dee03a9023a5
Bryant Perez Garcia <bryantperezgarcia005@gmail.com>
2024-08-12 16:23:56 -0400

    fix

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Novu } from '@novu/node';
+
 @Injectable()
 export class NotificationService {
   private readonly novu: Novu;
@@ -10,38 +11,25 @@ export class NotificationService {
 
   async sendNotification() {
     try {
-      // const user = await this.userService.getUserById(userId);
-      // if (!user) {
-      //   throw new Error(`User with ID ${userId} not found`);
-      // }
-
-      // const profile = user.profile;
-      // if (!profile) {
-      //   throw new Error(`Profile for user with ID ${userId} not found`);
-      // }
-      // const email = user.email;
-      // if (!email) {
-      //   throw new Error(`email not found`);
-      // }
-
-      // const formattedPhoneNumber = this.utilsService.validateNumber(
-      //   profile.phone,
-      // );
-
-      const response = await this.novu.trigger('demo-verify-otp', {
+      const response = await this.novu.trigger('demo-recent-login', {
         to: {
           subscriberId: '66b38f8faa4218d126b20170',
-          email: 'bryantperezgarcia005@gmail.com',
-          phone: 'formattedPhoneNumber,',
+          email: 'elokotoknik06@gmail.com',
         },
         payload: {
-          validationCode: 123456,
-          magicLinkURL: 'https://slack.com/magic/link',
-          __source: 'studio-test-workflow',
+          loginDate: '2024-08-07T13:45:06.404Z',
+          loginDevice: 'Desktop',
+          loginLocation: 'London, United Kingdom',
+          loginIP: '47.149.53.167',
+          userFirstName: 'Jane',
         },
       });
 
-      return response;
+      // Solo devuelve partes del objeto response que necesitas
+      return {
+        status: response.status,
+        data: response.data,
+      };
     } catch (error) {
       console.error('Error sending notification:', error);
       throw error;
