f6e01089a7c98b6381f3e8ec1b4706ef0e8b7cc2
Bryant Perez Garcia <bryantperezgarcia005@gmail.com>
2024-08-13 10:02:58 -0400

    trying novu

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,15 +1,14 @@
 import { Controller, Get, Request } from '@nestjs/common';
 
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { NotificationService } from './notification.service';
 
-@Controller('novu-notifications')
+@Controller('novu')
 export class NovuNotificationController {
   constructor(private readonly novuNotificationsService: NotificationService) {}
 
-  @ApiTags('notify-me')
-  @ApiBearerAuth()
-  @Get('send')
+  // @ApiTags('notify-me')
+  // @ApiBearerAuth()
+  @Get()
   async sendNotification(@Request() req: any) {
     // const userId = req.user.id;
     return this.novuNotificationsService.sendNotification();
