48011ec560337a924cef31d98476fa983d205219
Bryant Perez Garcia <bryantperezgarcia005@gmail.com>
2024-08-14 10:28:00 -0400

    type orm integration 🚀

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { QuotesModule } from './modules/quotes/quotes.module';
 import { NotificationModule } from './notification/notification.module';
 import { EmailModule } from './email/email.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import typeorm from './config/type.orm';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
+    ConfigModule.forRoot({ isGlobal: true, cache: true, load: [typeorm] }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) =>
+        configService.get('typeorm'),
     }),
     QuotesModule,
     NotificationModule,
