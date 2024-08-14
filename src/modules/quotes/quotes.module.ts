dc897eaf05ef8adf3bb7e3a1dfcfddfef6b9aa2d
Bryant Perez Garcia <bryantperezgarcia005@gmail.com>
2024-08-14 10:04:54 -0400

    ⬆️ FEAT mailer working 📩

--- src/modules/quotes/quotes.module.ts
+++ src/modules/quotes/quotes.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { QuotesService } from './quotes.service';
 import { QuotesController } from './quotes.controller';
 import { HttpModule } from '@nestjs/axios';
+import { EmailService } from 'src/email/email.service';
 
 @Module({
   imports: [HttpModule],
   controllers: [QuotesController],
-  providers: [QuotesService],
+  providers: [QuotesService, EmailService],
 })
 export class QuotesModule {}
