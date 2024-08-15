8b7006588f586c7babe6d22d0ef591954bd6ff56
Bryant Perez Garcia <bryantperezgarcia005@gmail.com>
2024-08-15 09:54:14 -0400

    cleaing code 🫧

--- src/modules/quotes/quotes.module.ts
+++ src/modules/quotes/quotes.module.ts
@@ -4,10 +4,11 @@ import { QuotesController } from './quotes.controller';
 import { HttpModule } from '@nestjs/axios';
 import { EmailService } from 'src/email/email.service';
 import { UsersModule } from '../users/users.module';
+import { EmailModule } from 'src/email/email.module';
 
 @Module({
-  imports: [HttpModule, UsersModule],
+  imports: [HttpModule, UsersModule, EmailModule],
   controllers: [QuotesController],
-  providers: [QuotesService, EmailService],
+  providers: [QuotesService],
 })
 export class QuotesModule {}
--- src/modules/quotes/quotes.service.ts
+++ src/modules/quotes/quotes.service.ts
@@ -1,11 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Request } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { UsersService } from '../users/users.service';
+import { EmailService } from 'src/email/email.service';
 
 @Injectable()
 export class QuotesService {
-  constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly userService: UsersService,
+    private readonly emailService: EmailService,
+  ) {}
+
+  async sendRandomQuote(@Request() req: any) {
+    const userId = req.user.id;
+    const user = await this.userService.getUserById(userId);
+    const quote = await this.getRandomQuote().toPromise();
+    const email = user.email;
+
+    await this.emailService.sendQuoteEmail(email, quote, req);
+  }
 
   getRandomQuote(): Observable<any> {
     return this.httpService
